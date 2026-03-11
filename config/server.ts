d3de6f0b8a9b5f2f6518ea6b00cc7d5db2d457cd
emachinesomar <omarmonasterio19@gmail.com>
2026-03-11 13:32:10 +0100

    ajuste url para vercel

--- config/server.ts
+++ config/server.ts
@@ -3,9 +3,12 @@ import type { Core } from '@strapi/strapi';
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('RENDER_EXTERNAL_URL') || env('VERCEL_URL') || env('URL') || 'http://localhost:1337',
+  url: env('PUBLIC_URL') || env('RENDER_EXTERNAL_URL') || env('VERCEL_URL') || env('URL') || 'http://localhost:1337',
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
 
